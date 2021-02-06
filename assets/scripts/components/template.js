920e7f3e3634c8a4b4c69be34c95f3978c232c3d
ysaaac <cyysabel@gmail.com>
2021-02-05 23:29:49 -0500

    logout event added

--- assets/scripts/components/template.js
+++ assets/scripts/components/template.js
@@ -1,11 +1,28 @@
-import { getToken } from "../data.js";
+import { getToken, removeToken } from "../data.js";
+import { logout } from "../services/sessions_fetch.js"
+
+let logoutClickListener = () => {
+  const parent = document.querySelector("#app");
+  parent.addEventListener("click", async (e)=>{
+    const logoutBtn = document.querySelector(".js-logout-btn");
+    if (e.target === logoutBtn) {
+      e.preventDefault();
+      await logout();
+      removeToken();
+      window.location = "/";
+    }
+  })
+}
+
+logoutClickListener()
+
 
 function PageTemplate(title, body) {
   const divApp = document.querySelector("#app");
   divApp.innerHTML = `
     <header class="header-main header--line js-header">
       <h2 class="form-title">${title}</h2>
-      ${getToken() ? `<a href="">Logout</a>` : ""}
+      ${getToken() ? `<a class="js-logout-btn" href="">Logout</a>` : ""}
     </header>
 
     <main>
